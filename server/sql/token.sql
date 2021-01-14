bfe7435123015c1ab5584239804ddf13448c1ff4
fanwen <1399701437@qq.com>
2021-01-14 15:00:14 +0800

    更新数据库

--- server/sql/token.sql
+++ server/sql/token.sql
@@ -1,1 +1,2 @@
-INSERT INTO `token`(`token`, `time`) VALUES ('admin', '1610349705434');
+INSERT INTO `token`(`token`, `time`) VALUES ('suzhou', '1610606521193');
+INSERT INTO `token`(`token`, `time`) VALUES ('admin', '1610606804533');
